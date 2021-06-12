a34a7503a5e5fbef68d4e01a427d206e3b41d01f
Naim Zaman <egnaimur@gmail.com>
2021-06-12 20:46:41 +0600

    Data Passing Way Two

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['Jasim','Deepjol','BappaRaz']	
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+		<header className="App-header">
+			<h1 style={{color:'red'}}>Dynamic / Static [Fixed] Data Passing</h1><hr></hr>
+			{/*====>>Componenet Call [Start]*/}
+				<Nayok name={nayoks[0]} age="56"></Nayok>
+				<Nayok name='Salman Sha'></Nayok>
+				<Nayok name={nayoks[2]}></Nayok>
+				<Nayok></Nayok>
+			{/*====>>Componenet Call [End]*/}
+		</header>
+	</div>
   );
 }
+//========>>Create Component [Start]
+function Nayok(props){
+	return (
+		<div>
+			<h1>Ami Khol Nayok: <span style={{color: 'rgb(123, 3, 252)'}}>{props.name || 'Elias Kanchon'}</span> </h1>
+			<h3>I have done 5 movies in <span style={{color:'rgba(88, 156, 59)'}}>{props.age || 20}</span> years </h3>
+		</div>
+	)
+}
+//========>>Create Component [End]
 
 export default App;
